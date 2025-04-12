8533b408888a491239ce53a0e7c7e5b2a6f6e26a
edsonbastos2 <edsonbastos2@hotmail.com>
2025-04-11 21:18:39 -0300

    fisrt commit

---
+++ src/test/java/com/geladaExpress/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geladaExpress.CadastroDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
